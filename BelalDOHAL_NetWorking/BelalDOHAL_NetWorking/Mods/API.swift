5d60f4b300f6cc8a710667f4225a06b120896e4d
Belal <Bealal1995Dohal@gmail.com>
2021-11-25 20:34:18 +0300

    Belal API Network: Digmons Images and Table View

---
+++ BelalDOHAL_NetWorking/BelalDOHAL_NetWorking/Mods/API.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Digmon: Codable {
+    var name: String
+    var img: String
+    var level: String
+}
---
+++ BelalDOHAL_NetWorking/BelalDOHAL_NetWorking/Views/ViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+class ViewController: UIViewController {
+    @IBOutlet weak var digmonLevel: UILabel!
+    @IBOutlet weak var digmonName: UILabel!
+    @IBOutlet weak var digmonImage: UIImageView!
+    @IBOutlet weak var digmonTabel: UITableView!
+    var digmonDataAPI = [Digmon]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        digmonsData {
+            self.digmonTabel.reloadData()
+            if let digoImageData = URL(string: self.digmonDataAPI[0].img) {
+                if let imageData = try? Data(contentsOf: digoImageData) {
+                    self.digmonImage.image = UIImage(data: imageData)
+                }
+            }
+            self.digmonName.text = self.digmonDataAPI[0].name
+            self.digmonLevel.text = self.digmonDataAPI[0].level
+        }
+        digmonTabel.delegate = self
+        digmonTabel.dataSource = self
+    }
+    //download: @escaping () -> ())
+    func digmonsData(download: @escaping () -> ()) {
+        if let urlResponce = URL(string:"https://digimon-api.vercel.app/api/digimon") {
+            let urlSession = URLSession(configuration: .default)
+            let urlTask = urlSession.dataTask(with: urlResponce) { data, response, error in
+                if let error = error {
+                    print("Error in the begening | The URL is bad",error.localizedDescription)
+                }else {
+                    if let digmoData = data {
+                        do {
+                            let decorder = JSONDecoder()
+                            self.digmonDataAPI = try decorder.decode([Digmon].self, from: digmoData)
+                            DispatchQueue.main.async {
+                                download()
+                            }
+                        } catch {
+                            print("Somthing wrong at end | The data not the same", error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            urlTask.resume()
+        }
+    }
+}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return digmonDataAPI.count
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let digmonCell = digmonTabel.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var digmonCellContain = digmonCell.defaultContentConfiguration()
+        //        digmonCellContain.image = UIImage(data: digmonDataAPI[indexPath.row].img)
+        if let digoImageData = URL(string: self.digmonDataAPI[indexPath.row].img) {
+            if let imageData = try? Data(contentsOf: digoImageData) {
+                digmonCellContain.image = UIImage(data: imageData)
+            }
+        }
+        digmonCellContain.imageProperties.maximumSize = CGSize(width: 100, height: 100)
+        digmonCellContain.text = digmonDataAPI[indexPath.row].name
+        digmonCellContain.secondaryText = digmonDataAPI[indexPath.row].level
+        digmonCell.contentConfiguration = digmonCellContain
+        return digmonCell
+    }
+}
+
