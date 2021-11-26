9ebcf4975f198c1b87e6a927806bdbb93ae938a5
Doaa <doaa.ageel@gmail.com>
2021-11-26 10:38:13 +0300

    display image,name,level

---
+++ doaaalageel_lap22_API/doaaalageel_lap22_API/Controller/modle/Digmon.swift
@@ -0,0 +1,13 @@
+//
+//  Digmon.swift
+//  doaaalageel_lap22_API
+//
+//  Created by Dua'a ageel on 19/04/1443 AH.
+//
+
+import Foundation
+struct Digmon: Codable {
+    var name:String
+    var img:String
+    var level:String
+}
