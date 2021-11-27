309d350bd8cef7f77aafc9783b902534c375bd6d
Faten Abdullh salem <faten@MacBook-Pro-Faten.local>
2021-11-27 17:10:34 +0300

    labe22

---
+++ FatanLabe22/FatanLabe22/Model/Digimon.swift
@@ -0,0 +1,13 @@
+//
+//  Digimon.swift
+//  FatanLabe22
+//
+//  Created by Faten Abdullh salem on 22/04/1443 AH.
+//
+
+import Foundation
+struct Digimon:Codable{
+var img:String
+var name:String
+var level:String
+}
