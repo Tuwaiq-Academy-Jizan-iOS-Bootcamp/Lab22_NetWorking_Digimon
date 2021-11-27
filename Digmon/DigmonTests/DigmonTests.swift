a1d35762df053737a11cecbab8d0b7121a1caf8e
MUSAMASMALI <musa14077@yahoo.com>
2021-11-27 22:20:34 +0300

    musa

---
+++ Digmon/DigmonTests/DigmonTests.swift
@@ -0,0 +1,33 @@
+//
+//  DigmonTests.swift
+//  DigmonTests
+//
+//  Created by موسى مسملي on 24/11/2021.
+//
+
+import XCTest
+@testable import Digmon
+
+class DigmonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
