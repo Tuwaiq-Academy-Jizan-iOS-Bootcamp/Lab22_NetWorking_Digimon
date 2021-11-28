b5a624e60f0399bb293b81b340698cad9a842bdb
AMALJELI <Aml3mr@hotmail.com>
2021-11-28 12:08:19 +0300

    Amal jeli - lab 22 digimon

---
+++ Amal jeli |_ Networking/Amal jeli |_ Networking/Digmon.swift
@@ -0,0 +1,14 @@
+//
+//  Digmon.swift
+//  Amal jeli |_ Networking
+//
+//  Created by Amal Jeli on 20/04/1443 AH.
+//
+
+import Foundation
+struct Digmon : Codable {
+    var name:String
+    var img:String
+    var level:String
+    
+}
---
+++ Amal jeli |_ Networking/Amal jeli |_ Networking/model.swift
@@ -0,0 +1,8 @@
+//
+//  model.swift
+//  Amal jeli |_ Networking
+//
+//  Created by Amal Jeli on 19/04/1443 AH.
+//
+
+import Foundation
