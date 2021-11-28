e9bb8d8b8047ff9007fd408154d64de4263dc7cb
hamad <7amad7arisi@gmail.com>
2021-11-28 21:06:50 +0300

    HamadHarisi_Lab22_Networking_Digimon

---
+++ HamadHarisi_Networking_Digimon_Lab22/HamadHarisi_Networking_Digimon_Lab22Tests/HamadHarisi_Networking_Digimon_Lab22Tests.swift
@@ -0,0 +1,33 @@
+//
+//  HamadHarisi_Networking_Digimon_Lab22Tests.swift
+//  HamadHarisi_Networking_Digimon_Lab22Tests
+//
+//  Created by حمد الحريصي on 24/11/2021.
+//
+
+import XCTest
+@testable import HamadHarisi_Networking_Digimon_Lab22
+
+class HamadHarisi_Networking_Digimon_Lab22Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
