fd9fab46e7710ae0e8f1681d7192861b773579aa
DAHMAJABER <dahma.alwani.jiz.s@tuwaiq.edu.sa>
2021-11-29 00:34:00 +0300

    finish

---
+++ Dahma_Digimon/Dahma_Digimon/Model/Digimon.swift
@@ -0,0 +1,14 @@
+//
+//  Digimon.swift
+//  Dahma_Digimon
+//
+//  Created by dahma alwani on 21/04/1443 AH.
+//
+
+import Foundation
+
+struct Digimon: Codable {
+    var name:String
+    var img:String
+    var level:String
+}
