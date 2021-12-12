b022d29aab1fe37b0a7112de14859a40a630456f
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-12-12 09:28:13 +0300

    bushra

--- Bushra Barakat_22/Bushra Barakat_22/Controller/ViewController.swift
+++ Bushra Barakat_22/Bushra Barakat_22/Controller/ViewController.swift
@@ -10,28 +10,28 @@ import UIKit
 class ViewController: UIViewController {
     var digmons: [Digmon] = []
     @IBOutlet weak var tableView: UITableView!
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         getData(with: "/digimon")
-       tableView.dataSource = self
-       tableView.delegate = self
-       
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         
         
         
     }
-
+    
     func getData(with endPoint: String){
         print ("is this work??")
         let baseURL = "https://digimon-api.vercel.app/api"
-//        1- creat url
+        //        1- creat url
         if let url = URL(string: baseURL + endPoint) {
             print("was the URL in correct formate")
-//        2- creat a URL Session
+            //        2- creat a URL Session
             let session = URLSession(configuration: .default)
-//        3- creat a task
+            //        3- creat a task
             let task = session.dataTask(with: url) { data, response, error in
                 if let error = error {
                     print("ERROR", error.localizedDescription)
@@ -53,42 +53,43 @@ class ViewController: UIViewController {
                     }
                 }
             }
-//          4 -
+            //          4 -
             task.resume()
         }
         
     }
-      
+    
     
 }
 extension ViewController: UITableViewDataSource,UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return digmons.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CellViewController
         cell.digamonNameLabel.text = digmons[indexPath.row].name
         cell.digmonLevleLabel.text = digmons[indexPath.row].level
-
+        cell.digmonImageView.image = nil
         if let imageURL = URL(string: digmons[indexPath.row].img) {
             DispatchQueue.global().async {
-                let data = try? Data(contentsOf: imageURL)
-                if let data = data {
+                if let data = try? Data(contentsOf: imageURL){
                     let image = UIImage(data: data)
                     DispatchQueue.main.async {
+                        if tableView.cellForRow(at: indexPath) != nil {
                         cell.digmonImageView.image = image
                     }
                 }
+                }
             }
-          
+            
             
         }
         
-
-    return cell
+        
+        return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
